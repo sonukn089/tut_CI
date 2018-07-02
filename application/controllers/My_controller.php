7fc293da0323a53ccecb2f032cc3a53f19b81995
Sonu <sonukn089@gmail.com>
2018-07-03 00:22:14 +0530

    add funtion in my controller

--- application/controllers/My_controller.php
+++ application/controllers/My_controller.php
@@ -8,6 +8,10 @@
 			//echo "<h1>This is my CI_Controller...</h1>";
 			$this->load->view('myindexview');
 		}
+		public function test(){
+
+			echo "<h2>THis is the test fuction from My Controller...</h2>";
+		}
 	}
 
 ?>
