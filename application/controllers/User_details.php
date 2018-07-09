72d9bc8197022085585dc72d2e1a5d3e6df909d9
Sonu <sonukn089@gmail.com>
2018-07-10 00:10:30 +0530

    create a helpers

--- application/controllers/User_details.php
+++ application/controllers/User_details.php
@@ -6,6 +6,10 @@
 
 			// echo "Test";
 			
+			$this->load->helper('common');
+
+			echo add();
+			
 			$this->load->model('user_model');        //load model Name
 			$data['userArray'] = $this->user_model->return_users();       // function Name
 			$this->load->view('user_view', $data);
---
+++ application/helpers/common_helper.php
@@ -0,0 +1,8 @@
+<?php
+
+	function add(){
+
+		return 10+5;
+	}
+
+?>
