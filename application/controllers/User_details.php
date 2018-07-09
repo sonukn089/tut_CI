a301aee8a6d57532754e23b9b2387bf3ce67c833
Sonu <sonukn089@gmail.com>
2018-07-09 23:35:24 +0530

    remove something

--- application/controllers/User_details.php
+++ application/controllers/User_details.php
@@ -3,8 +3,9 @@
 	class User_details extends CI_controller{
 
 		function index(){
-			
+
 			// echo "Test";
+			
 			$this->load->model('user_model');        //load model Name
 			$data['userArray'] = $this->user_model->return_users();       // function Name
 			$this->load->view('user_view', $data);
