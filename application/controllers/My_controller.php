80f7f9918014f44601e30b5e2ba15a3471f5456b
Sonu <sonukn089@gmail.com>
2018-07-19 23:39:42 +0530

    add something

---
+++ application/controllers/My_controller.php
@@ -0,0 +1,14 @@
+<?php
+	
+	class My_controller extends CI_controller{
+
+		public function index(){
+
+			echo "This is my controller.."."</br>";
+			echo "This is my controller.."."</br>";
+			echo "This is my controller.."."</br>";
+			echo "This is my controller..";
+		}
+	}
+
+?>
